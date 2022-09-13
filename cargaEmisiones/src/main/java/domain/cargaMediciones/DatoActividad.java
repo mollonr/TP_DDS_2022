1e9e45960413fdfaa8bef5f69f38e3e3fd8b7efe
mollonr <mollonr@gmail.com>
2022-09-12 22:26:37 -0300

    Delete cargaEmisiones directory

--- cargaEmisiones/src/main/java/domain/cargaMediciones/DatoActividad.java
+++
@@ -1,9 +0,0 @@
-package domain.cargaMediciones;
-
-public class DatoActividad {
-    private Actividad actividad;
-    private TipoConsumo tipoConsumo;
-    private float valor;
-    private String periodicidad;
-    private String periodoImputacion;
-}
